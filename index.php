50031788b3a816f772e4984b0b9da10520fba41c
canewman674 <canewman674@mail.cfcc.edu>
2025-11-24 19:40:52 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Index</title>
+    
+    <link rel="stylesheet" href="main.css">
+</head>
+<body>
+
+<h1>Project: Searching a Database with PHP and MySQL</h1>
+
+<form method="POST" action="results.php">
+    <label for="email">Student Email:</label>
+    <input type="email" name="email" id="email" required>
+    <input type="submit" value="Search">
+    <input type="email" required>
+</form>
+
+<p>Colin Newman<p>
+
+<p>11/24/2025<p>
+
+</body>
+</html>
